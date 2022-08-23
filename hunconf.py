df81b782098380f6fdd74d53632afb16a59c6f01
CV51GO <CV51GO@petalmail.com>
2022-08-23 19:39:59 +0800

    更新hubconf.py

--- hunconf.py
+++ hunconf.py
@@ -1,11 +1,11 @@
 
-from paa import PAA
+from paa import PAA as modelPAA
 import megengine.hub as hub
 
 
 @hub.pretrained(
 "https://studio.brainpp.com/api/v1/activities/3/missions/55/files/044e5766-6db9-4d7c-8f80-b3f30ff8b211"
 )
-def get_megengine_hardnet_model(pretrained=False):
-    model_megengine = PAA()
+def PAA(pretrained=False):
+    model_megengine = modelPAA()
     return model_megengine
